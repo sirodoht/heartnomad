540d588d04ddd6df7445ea44804a7565f1f50910
Theodore Keloglou <zf@sirodoht.com>
2024-05-29 13:59:01 +0100

    ruff format modernomad module

--- modernomad/core/management/commands/run_daily_tasks.py
+++ modernomad/core/management/commands/run_daily_tasks.py
@@ -5,7 +5,7 @@ from gather import tasks as gather_tasks
 
 
 class Command(BaseCommand):
-    help = 'Run daily scheduled tasks from Heroku Scheduler, or similar.'
+    help = "Run daily scheduled tasks from Heroku Scheduler, or similar."
 
     def handle(self, *args, **kwargs):
         tasks.generate_subscription_bills()
@@ -15,6 +15,5 @@ class Command(BaseCommand):
         tasks.send_departure_email()
         tasks.slack_embassysf_daily()
         gather_tasks.events_today_reminder()
-        if datetime.date.today().weekday() == 6: # sunday
+        if datetime.date.today().weekday() == 6:  # sunday
             gather_tasks.weekly_upcoming_events()
-
