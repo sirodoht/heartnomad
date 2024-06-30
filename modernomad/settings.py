3d66a1e0fe893f65208fd29502513b9bb9283ef8
Theodore Keloglou <zf@sirodoht.com>
2024-06-30 06:17:07 +0100

    remove unneeded template path on settings

--- modernomad/settings.py
+++ modernomad/settings.py
@@ -110,7 +110,6 @@ TEMPLATES = [
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
             BASE_DIR / "templates",
-            # BASE_DIR / "modernomad" / "core" / "templates",
         ],
         "APP_DIRS": True,
         "OPTIONS": {
