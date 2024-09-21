4c41bec92b4f76222b2a7c0a6d5eeb7049c1f752
Theodore Keloglou <zf@sirodoht.com>
2024-09-22 00:07:30 +0100

    Refactor user and main urls

--- modernomad/urls/main.py
+++ modernomad/urls/main.py
@@ -4,35 +4,43 @@ from django.contrib import admin
 from django.urls import include, re_path
 from django.views.generic import RedirectView
 
-import gather.views
-import modernomad.views
+from core.views import membership as membership_views
+from gather import views as gather_views
+from modernomad import views as modernomad_views
 
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(r"^$", modernomad.views.index),
-    re_path(r"^about/$", modernomad.views.about),
-    re_path(r"^host/$", modernomad.views.host, name="host"),
-    re_path(r"^stay/$", modernomad.views.stay),
-    re_path(r"^404/$", modernomad.views.ErrorView),
+    # Core views
+    re_path(r"^$", modernomad_views.index),
+    re_path(r"^about/$", modernomad_views.about),
+    re_path(r"^host/$", modernomad_views.host, name="host"),
+    re_path(r"^stay/$", modernomad_views.stay),
+    re_path(r"^404/$", modernomad_views.ErrorView),
     re_path(r"^admin/", admin.site.urls),
+    re_path(r"^membership/$", membership_views.manage, name="membership_manage"),
+    re_path(
+        r"^membership/charge_membership/$",
+        membership_views.charge,
+        name="membership_charge",
+    ),
     re_path(r"^people/", include("modernomad.urls.user")),
     re_path(r"^locations/", include("core.urls.location")),
-    re_path(r"^events/$", gather.views.upcoming_events_all_locations),
+    re_path(r"^events/$", gather_views.upcoming_events_all_locations),
     re_path(
         r"^events/emailpreferences/(?P<username>[\w\d\-\.@+_]+)/$",
-        gather.views.email_preferences,
+        gather_views.email_preferences,
         name="gather_email_preferences",
     ),
     re_path(r"^accounts/", include("bank.urls")),
-    re_path(r"^drft/$", modernomad.views.drft),
-    # various other useful things
+    re_path(r"^drft/$", modernomad_views.drft),
+    # Utility views
     re_path(
         r"^favicon\.ico$",
         RedirectView.as_view(url="/static/img/favicon.ico", permanent=True),
     ),
-    re_path(r"^robots\.txt$", modernomad.views.robots),
-    # api things
+    re_path(r"^robots\.txt$", modernomad_views.robots),
+    # API
     re_path(r"^api/", include("api.urls")),
     re_path(r"^", include("graphapi.urls")),
 ]
--- modernomad/urls/user.py
+++ modernomad/urls/user.py
@@ -1,74 +1,64 @@
 from django.contrib.auth import views as auth_views
 from django.urls import re_path
 
-import gather.views
-from core.views.billing import (
-    PeopleDaterangeQuery,
-    checkout_success,
-    create_checkout_session,
-    user_delete_card,
-)
+from core.views import billing as billing_views
+from core.views import user as user_views
 from core.views.booking import UserBookings
 from core.views.redirects import old_user_bookings_redirect
-from core.views.user import (
-    ListUsers,
-    UserAvatar,
-    UserDetail,
-    UserEdit,
-    email_available,
-    register,
-    user_edit_room,
-    user_email_settings,
-    user_events,
-    user_login,
-    username_available,
-)
-
-# Add the user registration and account management patters from the
-# django-registration package, overriding the initial registration
-# view to collect our additional user profile information.
-# urlpatterns = patterns('',
-#    re_path(r'^register/$', Registration.as_view(form_class = core.forms.UserProfileForm), name='registration_register'),
-# )
+from gather import views as gather_views
 
 urlpatterns = [
-    re_path(r"^$", ListUsers, name="user_list"),
-    re_path(r"^login/$", user_login, name="user_login"),
-    re_path(r"^check/email$", email_available, name="email_available"),
-    re_path(r"^check/username$", username_available, name="username_available"),
-    re_path(r"^register/$", register, name="registration_register"),
-    re_path(r"^daterange/$", PeopleDaterangeQuery, name="people_daterange"),
+    re_path(r"^$", user_views.ListUsers, name="user_list"),
+    re_path(r"^login/$", user_views.user_login, name="user_login"),
+    re_path(r"^check/email$", user_views.email_available, name="email_available"),
+    re_path(
+        r"^check/username$", user_views.username_available, name="username_available"
+    ),
+    re_path(r"^register/$", user_views.register, name="registration_register"),
+    re_path(
+        r"^daterange/$", billing_views.PeopleDaterangeQuery, name="people_daterange"
+    ),
     re_path(
         r"^(?P<username>(?!logout)(?!login)(?!register)(?!check)[\w\d\-\.@+_]+)/$",
-        UserDetail,
+        user_views.UserDetail,
         name="user_detail",
     ),
-    re_path(r"^(?P<username>[\w\d\-\.@+_]+)/avatar/$", UserAvatar, name="user_avatar"),
-    re_path(r"^(?P<username>[\w\d\-\.@+_]+)/edit/$", UserEdit, name="user_edit"),
+    re_path(
+        r"^(?P<username>[\w\d\-\.@+_]+)/avatar/$",
+        user_views.UserAvatar,
+        name="user_avatar",
+    ),
+    re_path(
+        r"^(?P<username>[\w\d\-\.@+_]+)/edit/$", user_views.UserEdit, name="user_edit"
+    ),
     re_path(
         r"^(?P<username>[\w\d\-\.@+_]+)/create-checkout-session/$",
-        create_checkout_session,
+        billing_views.create_checkout_session,
         name="create_checkout_session",
     ),
     re_path(
         r"^(?P<username>[\w\d\-\.@+_]+)/checkout-success/$",
-        checkout_success,
+        billing_views.checkout_success,
         name="checkout_success",
     ),
     re_path(
         r"^(?P<username>[\w\d\-\.@+_]+)/deletecard/$",
-        user_delete_card,
+        billing_views.user_delete_card,
         name="user_delete_card",
     ),
     re_path(
         r"^(?P<username>[\w\d\-\.@+_]+)/email/$",
-        user_email_settings,
+        user_views.user_email_settings,
         name="user_email_settings",
     ),
-    re_path(r"^(?P<username>[\w\d\-\.@+_]+)/events/$", user_events, name="user_events"),
+    re_path(
+        r"^(?P<username>[\w\d\-\.@+_]+)/events/$",
+        user_views.user_events,
+        name="user_events",
+    ),
     re_path(
         r"^(?P<username>[\w\d\-\.@+_]+)/room/(?P<room_id>\d+)/$",
-        user_edit_room,
+        user_views.user_edit_room,
         name="user_edit_room",
     ),
     re_path(
@@ -106,7 +96,7 @@ urlpatterns = [
 urlpatterns += (
     re_path(
         r"^(?P<username>[\w\d\-\.@+_]+)/events/$",
-        gather.views.user_events,
+        gather_views.user_events,
         name="user_events",
     ),
 )
